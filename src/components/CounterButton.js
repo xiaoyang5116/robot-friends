57e9c80612a3c507bbf553d37797aedee9d31584
shawn <1443584075@qq.com>
2023-05-24 09:51:24 +0800

    优化

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -10,16 +10,16 @@ class CounterButton extends PureComponent {
 
   //below is not needed if we extend PureComponent
   //like we have above
-  shouldComponentUpdate(nextProps, nextState) {
-    if (this.state.count !== nextState.count) {
-      return true
-    }
-    return false
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   if (this.state.count !== nextState.count) {
+  //     return true
+  //   }
+  //   return false
+  // }
 
   updateCount = () => {
     this.setState(state => {
-      return {count: state.count + 1}
+      return { count: state.count + 1 }
     })
   }
 
